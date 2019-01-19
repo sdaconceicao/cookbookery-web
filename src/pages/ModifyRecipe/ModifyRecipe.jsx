27879d26cc2f527e99f3c22f812aeb687cf2ccf5
Stephen da Conceicao <daconcei472@gmail.com>
2019-01-19 03:00:18 -0500

    Add and delete working

--- src/pages/ModifyRecipe/ModifyRecipe.jsx
+++ src/pages/ModifyRecipe/ModifyRecipe.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import axios from "axios";
 import {FormattedMessage} from 'react-intl';
-import {Form, Forms} from "sad-shared-components";
+import {Form, Forms, arrayUtils} from "sad-shared-components";
 
 export class ModifyRecipe extends Component {
 
@@ -16,6 +16,8 @@ export class ModifyRecipe extends Component {
         this.props.match.params.id
             ? axios.get(`/recipes/${this.props.match.params.id}`)
                 .then(response=>{
+                    response.data.ingredients = arrayUtils.addIdsToArrayElements(response.data.ingredients);
+                    response.data.steps = arrayUtils.addIdsToArrayElements(response.data.steps);
                     this.setState({...response.data, loading: false});
                 }).catch(error=>{
                 console.error("ERROR in retrieving recipe", error);
@@ -37,19 +39,31 @@ export class ModifyRecipe extends Component {
 
     handleAddIngredient = () =>{
         const {ingredients} = this.state;
-        ingredients.push('');
+        ingredients.push({key: Math.floor(Math.random() * 1000+1), value: ''});
         this.setState({ingredients});
     };
     handleAddStep = () =>{
         const {steps} = this.state;
-        steps.push('');
+        steps.push({key: Math.floor(Math.random() * 1000+1), value: ''});
         this.setState({steps});
+    };
+
+    handleRemoveIngredient = (index) =>{
+        const {ingredients} = this.state,
+            newIngredients = [...ingredients.slice(0, index), ...ingredients.slice(index+1)];
+        this.setState({ingredients: newIngredients });
+    };
+
+    handleRemoveStep = (index) =>{
+        const {steps} = this.state,
+            newSteps = [...steps.slice(0, index), ...steps.slice(index+1)];
+        this.setState({steps: newSteps });
     };
 
     render() {
         const {creating, loading, desc, title, ingredients, steps} = this.state;
         return (
-            <div className='modify-recipe'>
+            <div className='modify-recipe col-12'>
                 <h2>
                     {creating
                         ? <FormattedMessage id='recipe.create'/>
@@ -67,39 +81,37 @@ export class ModifyRecipe extends Component {
                                 required={true}
                                 label={<FormattedMessage id="recipe.desc"/>}/>
 
+                    <Forms.Label><FormattedMessage id="recipe.ingredients"/></Forms.Label>
                     <Forms.Fieldset>
-                        <Forms.Label><FormattedMessage id="recipe.ingredients"/></Forms.Label>
-                        <ul className='modify-recipe__ingredient-list'>
+                            <ul>
                             {ingredients.map((ingredient, index)=>{
                                 return (
-                                    <li key={ingredient} className="ingredient-list__item">
-                                        <Forms.TextInput name={`ingredient${index}`}
-                                                        value={ingredient}
-                                                        />
+                                    <li key={ingredient.key}>
+                                    <Forms.TextInput name="ingredient" index={index}
+                                                     value={ingredient.value}/>
+                                        <button type="button" onClick={()=>this.handleRemoveIngredient(index)}>X</button>
                                     </li>
                                 )
                             })}
-                            <li className="ingredient-list__item">
-                                <Forms.Button onClick={this.handleAddIngredient}><FormattedMessage id="recipe.ingredients.add"/></Forms.Button>
-                            </li>
-                        </ul>
+                            </ul>
+                        <button type="button" onClick={this.handleAddIngredient}><FormattedMessage id="recipe.ingredients.add"/></button>
                     </Forms.Fieldset>
                     <Forms.Fieldset>
                         <Forms.Label><FormattedMessage id="recipe.steps"/></Forms.Label>
-                        <ul className='modify-recipe__steps-list'>
+                        <ol className='modify-recipe__steps-list'>
                             {steps.map((step, index)=>{
                                 return (
-                                    <li key={step} className="step-list__item">
-                                        <Forms.Textarea name={`step${index}`}
-                                                        value={step}
+                                    <li key={step.key} className="step-list__item">
+                                        <Forms.Textarea name="step" index={index}
+                                                        value={step.value}
                                         />
+                                        <button type="button" onClick={()=>this.handleRemoveStep(index)}>X</button>
                                     </li>
                                 )
                             })}
-                            <li className="step-list__item">
-                                <Forms.Button onClick={this.handleAddStep}><FormattedMessage id="recipe.steps.add"/></Forms.Button>
-                            </li>
-                        </ul>
+                        </ol>
+
+                        <Forms.Button onClick={this.handleAddStep}><FormattedMessage id="recipe.steps.add"/></Forms.Button>
                     </Forms.Fieldset>
                     <Forms.Button type="submit"><FormattedMessage id="common.save"/></Forms.Button>
                 </Form>
