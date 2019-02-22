82dca636c600e7bda44af9005bc769eb21fe59d4
Stephen da Conceicao <daconcei472@gmail.com>
2019-02-22 01:43:17 -0500

    Dep cleanup, updated header styling

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,16 @@
 import React, { Component, Fragment } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import {IntlProvider} from "react-intl";
 import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
+import './Styles/index.scss'; //Need to load bootstrap styles before page/component styles
+
 import MainNav from './MainNav';
+import Pages from 'Pages';
 import * as enMessages from '../translations/en.json';
 import {enMessages as sharedEnMessages} from 'sad-shared-components';
-import Pages from 'Pages';
 
-import './Styles/index.scss';
 import './App.scss';
 
 export class App extends Component {
