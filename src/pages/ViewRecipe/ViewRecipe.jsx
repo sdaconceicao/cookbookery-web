a4db2b40b52c618fa29a8a2c6f634822208c9c08
Stephen da Conceicao <daconcei472@gmail.com>
2019-03-18 01:15:14 -0400

    Responsive layout view recipe

--- src/pages/ViewRecipe/ViewRecipe.jsx
+++ src/pages/ViewRecipe/ViewRecipe.jsx
@@ -5,9 +5,10 @@ import {FormattedMessage} from 'react-intl';
 import MdEdit from "react-icons/lib/md/edit";
 import MdDelete from "react-icons/lib/md/delete";
 
-import {Button, ModalConfirm} from "sad-shared-components";
+import {BackgroundImage, Button, DurationView, ModalConfirm, TagsList, Spinner} from "sad-shared-components";
 
-import RecipeDetails from "Components/RecipeDetails";
+import Ingredients from "Components/Ingredients";
+import Steps from "Components/Steps";
 
 import './ViewRecipe.scss';
 
@@ -40,22 +41,43 @@ export class ViewRecipe extends Component {
 
     };
 
+    renderRecipe = ({id, title, desc, image, ingredients, servingSize, prepTime, cookTime, tags, steps}) => (
+        <Fragment>
+            <div className="col-sm-4 col-12 image-col">
+                <span className="view-recipe__controls btn-group">
+                    <Link className="btn primary view-recipe__edit" to={`/recipe/${id}/edit`}>
+                        <MdEdit/>
+                    </Link>
+                    <Button className="primary view-recipe__remove" onClick={this.handleRemoveConfirm}>
+                        <MdDelete/>
+                    </Button>
+                </span>
+                <BackgroundImage src={image} className="recipe__image"/>
+                <TagsList tags={tags}/>
+                <div className="d-none d-sm-block"><FormattedMessage id={'recipe.servingSize'}/><div>{servingSize}</div></div>
+                <div className="d-none d-sm-block"><FormattedMessage id={'recipe.prepTime'}/> <DurationView value={prepTime}/></div>
+                <div className="d-none d-sm-block"><FormattedMessage id={'recipe.cookTime'}/> <DurationView value={cookTime}/></div>
+            </div>
+            <div className="col-sm-8 col-12">
+                <h2 className="recipe__title">{title}</h2>
+                <div className="recipe__description" dangerouslySetInnerHTML={{__html:desc}}></div>
+                <div className="d-block d-sm-none"><FormattedMessage id={'recipe.servingSize'}/><div>{servingSize}</div></div>
+                <div className="d-block d-sm-none"><FormattedMessage id={'recipe.prepTime'}/> <DurationView value={prepTime}/></div>
+                <div className="d-block d-sm-none"><FormattedMessage id={'recipe.cookTime'}/> <DurationView value={cookTime}/></div>
+                <h3><FormattedMessage id='recipe.ingredients'/></h3>
+                <Ingredients ingredients={ingredients}/>
+                <h3><FormattedMessage id='recipe.steps'/></h3>
+                <Steps steps={steps}/>
+            </div>
+        </Fragment>
+    );
+
     render() {
         const {recipe, removeConfirm, resolve} = this.state;
         return (
-            <div className='view-recipe'>
-                {recipe &&
-                    <Fragment>
-                        <span className="view-recipe__controls btn-group">
-                            <Link className="btn primary view-recipe__edit" to={`/recipe/${recipe.id}/edit`}>
-                                <MdEdit/>
-                            </Link>
-                            <Button className="primary view-recipe__remove" onClick={this.handleRemoveConfirm}>
-                                <MdDelete/>
-                            </Button>
-                        </span>
-                        <RecipeDetails {...recipe}/>
-                    </Fragment>
+            <div className='view-recipe row'>
+                {recipe ?
+                    this.renderRecipe(recipe) : <Spinner/>
                 }
                 {removeConfirm &&
                     <ModalConfirm title={<FormattedMessage id={'recipe.remove.title'}/>} resolve={resolve}>
